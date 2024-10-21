32140307918f54749b7d14ac19874245cf0d29b9
Roman Obaraz <lexium729@gmail.com>
2024-10-21 12:04:41 +0300

    fix scale input validation

--- src/App.jsx
+++ src/App.jsx
@@ -33,9 +33,12 @@ export default function App() {
     function handleScaleInputChange(e) {
         let inputValue = e.target.value;
 
-        // Parse to integer to remove leading zeros
-        if (inputValue !== "" && inputValue > 0) {
-            inputValue = inputValue.replace(/^0+/, "") || "0"; // Remove leading zeros to prevent input like "01"
+        // Allow empty string so user can clear input
+        if (inputValue === "") {
+            setImageScale("");
+        } else if (inputValue > 0) {
+            // Remove leading zeros to prevent input like "01"
+            inputValue = inputValue.replace(/^0+/, "");
             setImageScale(inputValue);
         } else {
             setImageScale(1);
@@ -64,17 +67,21 @@ export default function App() {
 
             {fileData && (
                 <>
-                    <div>
+                    <ImageDownloader imageScale={imageScale} />
+                    <div id="scaleInput">
                         Scale:{" "}
                         <input
                             type="number"
+                            pattern="\d*"
                             min={1}
                             max={10}
                             value={imageScale}
                             onChange={handleScaleInputChange}
+                            onKeyDown={(e) =>
+                                ["e", "E", "+", "-", "."].includes(e.key) && e.preventDefault()
+                            }
                         />
                     </div>
-                    <ImageDownloader imageScale={imageScale} />
                     <PhotoGallery
                         fileData={fileData}
                         imageScale={imageScale}
