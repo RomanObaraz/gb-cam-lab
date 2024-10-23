f26434bad14eb8a244a99ea43cea7b3af4e45c45
Roman Obaraz <lexium729@gmail.com>
2024-10-23 10:34:27 +0300

    some code organization

--- src/components/ColorPalette.jsx
+++ src/components/ColorPalette.jsx
@@ -1,38 +1,18 @@
 export default function ColorPalette({ colors, onChange }) {
     return (
         <div id="colorPalette">
-            <div>
-                Color 1:{" "}
-                <input
-                    type="color"
-                    value={colors[0]}
-                    onChange={(e) => onChange(0, e.target.value)}
-                />
-            </div>
-            <div>
-                Color 2:{" "}
-                <input
-                    type="color"
-                    value={colors[1]}
-                    onChange={(e) => onChange(1, e.target.value)}
-                />
-            </div>
-            <div>
-                Color 3:{" "}
-                <input
-                    type="color"
-                    value={colors[2]}
-                    onChange={(e) => onChange(2, e.target.value)}
-                />
-            </div>
-            <div>
-                Color 4:{" "}
-                <input
-                    type="color"
-                    value={colors[3]}
-                    onChange={(e) => onChange(3, e.target.value)}
-                />
-            </div>
+            {colors.map((color, i) => {
+                return (
+                    <div key={`color-${i}`}>
+                        Color {i}:{" "}
+                        <input
+                            type="color"
+                            value={color}
+                            onChange={(e) => onChange(i, e.target.value)}
+                        />
+                    </div>
+                );
+            })}
         </div>
     );
 }
