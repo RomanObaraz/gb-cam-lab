ea97e0bb822c69d78fc087ba35860d564730cbe7
Roman Obaraz <lexium729@gmail.com>
2024-11-10 14:50:40 +0200

    fix ImageScaler

--- src/components/downloadControls/ImageScaler.jsx
+++ src/components/downloadControls/ImageScaler.jsx
@@ -5,24 +5,33 @@ import { PHOTO_HEIGHT, PHOTO_WIDTH } from "../../utils/constants";
 export default function ImageScaler() {
     const { imageScale, setImageScale } = useStore();
 
-    function handleScaleInputChange(inputValue) {
-        switch (inputValue) {
-            case "":
-                // do nothing
-                break;
-            case 0:
-                setImageScale(1);
-                break;
-            default:
-                setImageScale(inputValue);
-                break;
+    function handleScaleInputChange(e) {
+        let inputValue = e.target.value;
+
+        // Allow empty string so user can clear input
+        if (inputValue === "") {
+            setImageScale("");
+        } else if (inputValue > 0) {
+            // Remove leading zeros to prevent input like "01"
+            inputValue = inputValue.replace(/^0+/, "");
+            setImageScale(inputValue);
+        } else {
+            setImageScale(1);
         }
     }
 
     return (
-        <>
-            <TextField label="Scale" value={imageScale} onChange={handleScaleInputChange} />
+        <div>
+            <TextField
+                type="number"
+                label="Scale"
+                min={1}
+                max={10}
+                value={imageScale}
+                onChange={handleScaleInputChange}
+                onKeyDown={(e) => ["e", "E", "+", "-", "."].includes(e.key) && e.preventDefault()}
+            />
             <span>{` (${PHOTO_WIDTH * imageScale}x${PHOTO_HEIGHT * imageScale} px)`}</span>
-        </>
+        </div>
     );
 }
