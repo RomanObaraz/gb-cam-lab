09b8601c5a6cabc1cf5fed3288e6d63c72533447
Roman Obaraz <lexium729@gmail.com>
2024-12-05 14:04:16 +0200

    implement persisting imageScale setting

--- src/stores/useStore.js
+++ src/stores/useStore.js
@@ -1,9 +1,18 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useStore = create((set) => ({
-    fileData: null,
-    imageScale: 3,
+export const useStore = create(
+    persist(
+        (set) => ({
+            fileData: null,
+            imageScale: 3,
 
-    setFileData: (data) => set({ fileData: data }),
-    setImageScale: (scale) => set({ imageScale: scale }),
-}));
+            setFileData: (data) => set({ fileData: data }),
+            setImageScale: (scale) => set({ imageScale: scale }),
+        }),
+        {
+            name: "scale-storage",
+            partialize: (state) => ({ imageScale: state.imageScale }),
+        }
+    )
+);
